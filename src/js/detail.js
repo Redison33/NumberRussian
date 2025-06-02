bd7a0084bad52e97f46176689d8ee5030e6a70e4
suddenbl <sorryfoff@gmail.com>
2025-06-02 14:14:55 +0300

    404

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,4 +1,11 @@
 $('.party__slider').slick({
     slidesToShow: 1,
     variableWidth: true,
+    infinite: false,
+});
+
+$('.party__slider-1').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    infinite: false,
 });
